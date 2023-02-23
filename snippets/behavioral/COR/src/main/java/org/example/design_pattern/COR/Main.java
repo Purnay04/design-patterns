fc60974c67c531f8fb59a51486c4f03448fd12b3
purnay <bargeamanpurnay087@gmail.com>
2023-02-23 18:05:17 +0530

    Added snippets parent mvn project & COR behavioral pattern project

---
+++ snippets/behavioral/COR/src/main/java/org/example/design_pattern/COR/Main.java
@@ -0,0 +1,7 @@
+package org.example.design_pattern.COR;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ snippets/src/main/java/org/example/design_pattern_snip/Main.java
@@ -0,0 +1,7 @@
+package org.example.design_pattern_snip;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
